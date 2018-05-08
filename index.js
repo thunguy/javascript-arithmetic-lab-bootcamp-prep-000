ccfc2f763eaab0ab91bbde20add63af1178d3f75
thunguy@me.com <thunguy@me.com>
2018-05-08 01:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ function makeInt(n) {
     return parseInt(n, 10)
 }
 function preserveDecimal(n) {
+  if (n === n) {
     return parseFloat()
+  } else if (n === isNaN) {
+    return NaN
+  }  
 }
+
 
 
 
